4d47a0b5a6a99184d88593faf5519c977b5fd18f
Mathias03R <mathiasromero100@gmail.com>
2024-04-04 20:46:05 -0500

    se agregaron las respuestas en main

--- src/listadeejercicios01/ListaDeEjercicios01.java
+++ src/listadeejercicios01/ListaDeEjercicios01.java
@@ -3,7 +3,13 @@ package listadeejercicios01;
 public class ListaDeEjercicios01 {
 
     public static void main(String[] args) {
-        
+        //a) Customer - Order: Asociación
+        //b) Order - OrderDetail: Composición
+        //c) OrderDetail - Product: Agregación
+        //d) Order - Payment: Asociación
+        //e) Payment-Cash: Herencia
+        //f) Payment-Check: : Herencia
+        //g) Payment-CreditCard: Herencia
     }
     
 }
